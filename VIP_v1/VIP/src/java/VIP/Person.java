ad6a0632c25409549f767dad867e0b374fe89802
matthieudelaro <matthieudelaro@gmail.com>
2014-12-12 17:26:15 +0100

    save v1

---
+++ VIP_v1/VIP/src/java/VIP/Person.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VIP;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author adrien
+ */
+public class Person {
+
+    private String name; 
+    private String prenom;
+    private String telPerso;
+    private String telPro;
+    private String telDom;
+    private String address;
+    private String zipCode;
+    private String city;
+    private String email;
+
+    /* getters */
+    public String getName() {
+        return name;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getTelPerso() {
+        return telPerso;
+    }
+
+    public String getTelDom() {
+        return telDom;
+    }
+    
+    public String getTelPro() {
+        return telPro;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    /* setters */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setTelPerso(String telPerso) {
+        this.telPerso = telPerso;
+    }
+
+    public void setTelPro(String telPro) {
+        this.telPro = telPro;
+    }
+
+    public void setTelDom(String telDom) {
+        this.telDom = telDom;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /* ctors */
+    
+    public Person(String name, String prenom, String telPerso, String telPro, String telDom, String address, String zipCode, String city, String email) {
+        this.name = name;
+        this.prenom = prenom;
+        this.telPerso = telPerso;
+        this.telPro = telPro;
+        this.telDom = telDom;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.email = email;
+    }
+    
+    public Person ()
+    {
+        this.name = "default";
+        this.prenom = "default";
+        this.telPerso = "default";
+        this.telPro = "default";
+        this.telDom = "default";
+        this.address = "default";
+        this.zipCode = "default";
+        this.city = "default";
+        this.email = "default";
+    }
+    
+    public Person (ResultSet rs)
+    {
+        if (!inflate(rs)) {
+            throw new java.security.InvalidParameterException("Could not inflate.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", prenom=" + prenom + ", telPerso=" + telPerso + ", telPro=" + telPro + ", telDom=" + telDom + ", address=" + address + ", zipCode=" + zipCode + ", city=" + city + ", email=" + email + '}';
+    }
+
+    public Boolean inflate (ResultSet rs)
+    {
+        try
+        {
+            this.setName(       rs.getString("NOM")); 
+            this.setPrenom(     rs.getString("PRENOM")); 
+            this.setTelPerso(   rs.getString("TELPOR")); 
+            this.setTelPro(     rs.getString("TELPRO")); 
+            this.setTelDom(     rs.getString("TELDOM")); 
+            this.setAddress(    rs.getString("ADRESSE")); 
+            this.setZipCode(    rs.getString("CP")); 
+            this.setCity(       rs.getString("VILLE")); 
+            this.setEmail(      rs.getString("EMAIL"));
+        }
+        catch ( Exception e)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+    
+}
---
+++ VIP_v1/VIP/web/details.jsp
@@ -0,0 +1,67 @@
+<%-- 
+    Document   : details
+    Created on : 6 déc. 2014, 10:53:27
+    Author     : adrien
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="VIP.*"%>
+
+<jsp:useBean scope="request" class="VIP.Person" id="myPerson" />
+<%@page import="java.util.*"%>
+<%@page import="java.sql.*"%>
+<%@page import="com.sun.faces.*"%>
+
+<% 
+    String URL = "jdbc:derby://localhost:1527/VIP";
+    String USER = "efrei";
+    String PASS = "efrei";
+    Connection conn = DriverManager.getConnection(URL, USER, PASS); 
+    Statement stmt = conn.createStatement();
+%>
+
+
+<%
+    String ID = request.getParameter("ID");
+    ResultSet rs = stmt.executeQuery("select * from UTILISATEUR where NOM='" + ID + "'");
+    
+
+    if (rs.next()){
+        myPerson.inflate(rs);
+  
+    }
+        
+%>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>VIP - Details</title>
+    </head>
+    <body>
+        <h1>VIP's Details</h1>
+        
+        <p>
+    
+            Nom :    <input type="text" value="${myPerson.name}" readonly> 
+            Prenom : <input type="text" value="${myPerson.prenom}" readonly> </br>
+            </br>
+            <b>Numeros de telephone</b> </br>
+            Domicile :       <input type="text" value="${myPerson.telDom}" readonly>     </br>
+            Portable :       <input type="text" value="${myPerson.telPerso}" readonly>   </br>
+            Professionnel :  <input type="text" value="${myPerson.telPro}" readonly>     </br>
+            </br>
+            Adresse :       <input type="text" value="${myPerson.address}" readonly>    </br>
+            Code Postal :   <input type="text" value="${myPerson.zipCode}" readonly>  
+            Ville :         <input type="text" value="${myPerson.city}" readonly>       </br>
+            Adresse email : <input type="text" value="${myPerson.email}" readonly>    </br>
+        </p>
+        
+        <form action="search.jsp">
+            <input type="submit" value="retour" >
+        </form>
+        
+        
+    </body>
+</html>
